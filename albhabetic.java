32ff012f6d20b010c553b9c179305a1554d5771a
vimal2505 <43266761+vimal2505@users.noreply.github.com>
2018-10-05 11:57:53 +0530

    Add files via upload

---
+++ albhabetic.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+ char c = '*';
+ if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+ System.out.println("is an alphabet.");
+ else
+ System.out.println("not an alphabet.");
+  }
+}
